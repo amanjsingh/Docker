22008d55ed8ae7963964b620c83f5b3ffd609aa9
Amanjot Singh <am350894@dal.ca>
2023-02-01 20:02:55 -0400

    Local Working Containers

--- A1/Container1/validator.py
+++ A1/Container1/validator.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
 import os
+import requests
 
 app = Flask(__name__)
 
@@ -8,20 +9,22 @@ app = Flask(__name__)
 @app.route("/checksum", methods=["POST"])
 def validator():
     request_body = request.get_json()
-    print("\nDictionary :", request_body)
     if request_body:
         if "file" in request_body and request_body.get("file"):
             file_name = request_body.get("file")
+            path = "./data/" + file_name
+            if os.path.isfile(path):
+                hash = requests.post("http://calculator:3000/calculate", json=request_body)
+                request_body["checksum"] = hash.text
+            else:
+                request_body["error"] = "File not found."
         else:
             request_body["error"] = "Invalid JSON input."
-            print("\nJSON :", request_body)
-            return json.dumps(request_body)
     else:
         request_body = {}
         request_body["error"] = "Invalid JSON input."
-        return json.dumps(request_body)
 
-    return ""
+    return json.dumps(request_body)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
--- A1/Container2/md5-checksum.py
+++ A1/Container2/md5-checksum.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import json
 import os
 import hashlib
 
@@ -10,13 +9,12 @@ app = Flask(__name__)
 def calculate_checksum():
     request_body = request.get_json()
     if request_body and "file" in request_body:
-        file_name = request_body.get("file")
+        file_name = "./data/" + request_body.get("file")
         with open(file_name, "r") as file:
             content_bytes = file.read().encode()
             hash = hashlib.md5(content_bytes).hexdigest()
-            request_body["checksum"] = hash
-    return json.dumps(request_body)
+            return hash
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
