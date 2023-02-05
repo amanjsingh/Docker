57f59edf67671052d737d9b177adfc08a1d3052a
Amanjot Singh <am350894@dal.ca>
2023-02-05 10:52:38 -0400

    Added References

--- A1/Container1/validator.py
+++ A1/Container1/validator.py
@@ -1,3 +1,24 @@
+### References
+# [1]		“Python for Beginners – Full Course [Programming Tutorial].” YouTube, 
+#         www.youtube.com/watch?v=eWRfhZUzrAc.
+
+# [2]		Srivastav, Prakhar. “A Docker Tutorial for Beginners.” A Docker Tutorial for Beginners,
+#         docker-curriculum.com.
+
+# [3]		“Python Requests Post Method.”  _Python Requests Post Method_, 
+#         www.w3schools.com/python/ref_requests_post.asp.
+
+# [4]		“Quickstart — Requests 2.28.2 Documentation.”  _Quickstart — Requests 2.28.2 Documentation_, 
+#         requests.readthedocs.io/en/latest/user/quickstart.
+
+# [5]		“7. Input and Output.”  _Python Documentation_, 
+#         docs.python.org/3/tutorial/inputoutput.html.
+
+# [6]		“os.path — Common Pathname Manipulations.”  _Python Documentation_, 
+#         docs.python.org/3/library/os.path.html.
+
+
+
 from flask import Flask, request
 import json
 import os
